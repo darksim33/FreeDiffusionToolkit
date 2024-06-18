1d26aa575c14bf00101fc2f8ce1a5d3c323a38b3
thiel <57491748+darksim33@users.noreply.github.com>
2024-06-18 11:38:38 +0200

    fead: update vector plotter with inverted vectors

--- freediffusiontoolkit/vector_ploter.py
+++ freediffusiontoolkit/vector_ploter.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-def plot_vectors(vectors: np.array):
-
-    x = y = z = np.zeros((vectors.shape[0],))
-
+def plot_vectors(vectors: np.array, show_inverted: bool = False):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-    ax.quiver(
-        x,
-        y,
-        z,
-        np.squeeze(vectors[:, 0]),
-        np.squeeze(vectors[:, 1]),
-        np.squeeze(vectors[:, 2]),
-    )
+
+    if not show_inverted:
+        x = y = z = np.zeros((vectors.shape[0],))
+        u = np.squeeze(vectors[:, 0])
+        v = np.squeeze(vectors[:, 1])
+        w = np.squeeze(vectors[:, 2])
+        color = ["C0"] * vectors.shape[0]
+    else:
+        x = y = z = np.zeros((vectors.shape[0] * 2,))
+        u = np.concatenate((np.squeeze(vectors[:, 0]), np.squeeze(vectors[:, 0]) * -1))
+        v = np.concatenate((np.squeeze(vectors[:, 1]), np.squeeze(vectors[:, 1]) * -1))
+        w = np.concatenate((np.squeeze(vectors[:, 2]), np.squeeze(vectors[:, 2]) * -1))
+        color = ["C0"] * vectors.shape[0] + ["C1"] * vectors.shape[0]
+    ax.quiver(x, y, z, u, v, w, color=color)
+
     ax.set_xlim([-1, 1])
     ax.set_ylim([-1, 1])
     ax.set_zlim([-1, 1])
--- tests/test_vector_plotter.py
+++ tests/test_vector_plotter.py
@@ -6,10 +6,22 @@ from freediffusiontoolkit.vector_ploter import plot_vectors
 def test_vector_plotter_n_dims_3():
     diff_tool = FreeDiffusionTool([1000], 3)
     vectors = diff_tool.get_diffusion_vectors()
-    plot_vectors(vectors)
+    assert plot_vectors(vectors)
+
+
+def test_vector_plotter_n_dims_3_inverted():
+    diff_tool = FreeDiffusionTool([1000], 3)
+    vectors = diff_tool.get_diffusion_vectors()
+    assert plot_vectors(vectors, True)
 
 
 def test_vector_plotter_n_dims_4():
     diff_tool = FreeDiffusionTool([1000], 4)
     vectors = diff_tool.get_diffusion_vectors()
-    plot_vectors(vectors)
+    assert plot_vectors(vectors)
+
+
+def test_vector_plotter_n_dims_6():
+    diff_tool = FreeDiffusionTool([1000], 6)
+    vectors = diff_tool.get_diffusion_vectors()
+    assert plot_vectors(vectors)
