4187acab27581021b3efc5d258086c76fc4b30b6
thiel <57491748+darksim33@users.noreply.github.com>
2024-06-18 13:21:37 +0200

    fix: test assertion error

--- tests/test_vector_plotter.py
+++ tests/test_vector_plotter.py
@@ -6,22 +6,33 @@ from freediffusiontoolkit.vector_ploter import plot_vectors
 def test_vector_plotter_n_dims_3():
     diff_tool = FreeDiffusionTool([1000], 3)
     vectors = diff_tool.get_diffusion_vectors()
-    assert plot_vectors(vectors)
+    plot_vectors(vectors)
+    assert True
 
 
 def test_vector_plotter_n_dims_3_inverted():
     diff_tool = FreeDiffusionTool([1000], 3)
     vectors = diff_tool.get_diffusion_vectors()
-    assert plot_vectors(vectors, True)
+    plot_vectors(vectors, True)
+    assert True
 
 
 def test_vector_plotter_n_dims_4():
     diff_tool = FreeDiffusionTool([1000], 4)
     vectors = diff_tool.get_diffusion_vectors()
-    assert plot_vectors(vectors)
+    plot_vectors(vectors)
+    assert True
 
 
 def test_vector_plotter_n_dims_6():
     diff_tool = FreeDiffusionTool([1000], 6)
     vectors = diff_tool.get_diffusion_vectors()
-    assert plot_vectors(vectors)
+    plot_vectors(vectors)
+    assert True
+
+
+def test_vector_plotter_multi_b():
+    diff_tool = FreeDiffusionTool([0, 500, 1000], 4)
+    vectors = diff_tool.get_diffusion_vectors()
+    plot_vectors(vectors)
+    assert True
